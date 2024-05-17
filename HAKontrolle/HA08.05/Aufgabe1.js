f93a730e98fabc920307c0df6fcce3ab2cd30d38
Suheib Marzouka <suheibmarzouka@MBP-von-Suheib.fritz.box>
2024-05-17 13:29:34 +0200

    paar Beispiel Lösungen hinzugefügt der HA vom 08.05

---
+++ HAKontrolle/HA08.05/Aufgabe1.js
@@ -0,0 +1,11 @@
+// Aufgabe 1:
+// Schreibe ein Programm, mit einer Funktion “aufgabe1”.
+// Diese Funktion nimmt einen String entgegen und gibt diesen rückwärts in der Konsole aus.
+
+function aufgabe1(str) {
+  console.log(str.split(" ").reverse().join(" "));
+}
+
+aufgabe1("Hallo Wie geht es dir"); //  rid se theg eiW ollaH
+
+
