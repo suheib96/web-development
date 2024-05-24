ffec03e12a73373a799ff95624bb0118245da459
Suheib Marzouka <suheibmarzouka@MBP-von-Suheib.fritz.box>
2024-05-24 13:03:33 +0200

    restliche Änderung des heutigen Unterrichts hochgeladen 24-05

--- my-unterricht-24-05-24-react-app/src/App.js
+++ my-unterricht-24-05-24-react-app/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   // hooks müssen in einer functions Komponente aufgerufen werden
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    // Der Code der ausgeführt werden soll
+    console.log("Hallo aus dem UseEffect" + counter);
+  }, [counter]); // Dependency Array, wann soll der Code ausgeführt
 
   function increment() {
     setCounter(counter + 1);
@@ -11,11 +16,15 @@ function App() {
   function decrement() {
     setCounter(counter - 1);
   }
+  function reset() {
+    setCounter(0);
+  }
   return (
     <div className="container">
       <h1>{counter}</h1>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <button onClick={reset}>R</button>
     </div>
   );
 }
