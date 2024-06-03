d096aa2231164db2d845178c91b2ee8e50153c3b
Suheib Marzouka <suheibmarzouka@MBP-von-Suheib.fritz.box>
2024-06-03 10:25:46 +0200

    Beispiellösung für die HA vom Freitag 31.05 hinzugefügt

--- form-testing-react-app/src/App.test.js
+++ form-testing-react-app/src/App.test.js
@@ -1,7 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("ob ein neuer User abgeschickt werden kann und in der Liste angezeigt wird", async () => {
   render(<App />);
 
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const buttonElement = screen.getByRole("button", { name: /senden/i });
+
+  expect(nameInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(buttonElement).toBeInTheDocument();
+
+  await user.click(nameInput);
+  await user.keyboard("suheib");
+  await user.click(emailInput);
+  await user.keyboard("suheib@suheib.de");
+
+  await user.click(buttonElement);
+
+  const name = screen.getByRole("cell", { name: "suheib" });
+  const email = screen.getByRole("cell", { name: "suheib@suheib.de" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
