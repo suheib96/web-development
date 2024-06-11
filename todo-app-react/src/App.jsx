a7b1b11c110141ddcea4e334694d12efbc0d0cd4
MTeeeee <112711304+MTeeeee@users.noreply.github.com>
2024-06-11 13:41:51 +0200

    todo app react

---
+++ todo-app-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TodoList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
